04b45a547c28a3bce8dfed3998073ad9be06420f
Romazherdev <romazherdev@gmail.com>
2019-05-29 22:19:16 +0300

    cases

--- src/models/consts/my-works.ts
+++ src/models/consts/my-works.ts
@@ -16,55 +16,72 @@ export const MY_WORKS: IProject[] = [
         color: '#FEBE10',
         logo: 'img/my-works/burger-king.png',
         // link: 'https://bo-pb.com',
-        descr: '',
-        subtitle: '',
+        descr: 'A kiosk application for making purchases<br><br>' +
+            'Technolgoies:<br>' +
+            'HTML, Sass, Angular',
+        subtitle: 'Kiosk application',
     },
     {
         name: 'Mixort',
         color: '#4DC223',
         logo: 'img/my-works/mixort.png',
         link: 'https://mixort.com',
-        descr: '',
-        subtitle: '',
+        descr: 'Isomorphic website with admin panel.<br><br>' +
+            'Technologies:<br>' +
+            'Angular 7, Node.js, express, Angular Universal, Angular Material, MongoDB, NgRx, RxJS',
+        subtitle: 'SPA with SSR + CMS',
     },
     {
         name: 'Gaia',
         color: '#212121',
         logo: 'img/my-works/gaia.png',
         link: 'http://gaia.gml.aisnovations.com',
-        descr: '',
-        subtitle: '',
+        descr: 'Users are able to buy courses and pass them. Administrators can to create courses, tests, review them,' +
+            'solve issues which comes from users etc.<br><br>' +
+            'Technologies:<br>' +
+            'Angular 6, Node.js, express, Stripe, Angular Material, MongoDB, NgRx, RxJS',
+        subtitle: 'Online learning platform',
     },
     {
         name: 'NDAX',
         color: '#00B8FF',
         logo: 'img/my-works/ndax.png',
         link: 'http://ndax.gml.aisnovations.com',
-        descr: '',
-        subtitle: '',
+        descr: 'Buy/sell crypto or national currency. See everything in realtime. Do withdraw from your wallet' +
+            ' or make deposits.<br><br>' +
+            'Technologies:<br>' +
+            'Angular 7, Bootstrap, NgRx, RxJS, Angular Universal',
+        subtitle: 'Cryptocurrency trading platform',
     },
     {
         name: 'Galcon',
         color: '#00AEEC',
         logo: 'img/my-works/galcon.png',
         link: 'http://galileo.gml.aisnovations.com',
-        descr: '',
-        subtitle: '',
+        descr: 'This company is specialised on irrigation systems. The website I\'ve been working on gives you ' +
+            'an ability to configure your irrigation system however you want. You can create different programs ' +
+            'and make a schedule for them. You can see the status of your system in realtime: which valve is ' +
+            'irrigating at the moment, if there any alert or something like this.',
+        subtitle: 'Automated irrigation systems',
     },
     {
         name: 'JET Technical',
         color: '#086090',
         logo: 'img/my-works/jet.png',
         link: 'http://jet.gml.aisnovations.com',
-        descr: '',
-        subtitle: '',
+        descr: 'Isomorphic website with admin panel.<br><br>' +
+            'Technologies:<br>' +
+            'Angular 7, Node.js, express, Angular Universal, Angular Material, MongoDB, NgRx, RxJS',
+        subtitle: 'SPA with SSR + CMS',
     },
     {
         name: 'Via',
         color: '#778CFD',
         logo: 'img/my-works/via.png',
         link: 'https://viacustomer.com',
-        descr: '',
-        subtitle: '',
+        descr: 'You can connect your Google/Facebook account. The website provides you statistics of reviews,' +
+            ' it also has SMS messenger. You can send review invites this via messenger, customize the layout of ' +
+            'invite, schedule notifications for your clients, like discounts on it\'s birthday and many other things.',
+        subtitle: 'A tool for business',
     },
 ];
